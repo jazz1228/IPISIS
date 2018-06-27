2bc7ba1c778be771033406c18a4506aed6378e0f
jazz1228 <juan.zapata19@udea.edu.co>
2018-06-26 20:22:40 -0500

    example

--- IPISISv.1/api/controllers/PrerrequisitoController.js
+++
@@ -1,9 +0,0 @@
-/**
- * PrerrequisitoController
- *
- * @description :: Server-side logic for managing prerrequisitoes
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-module.exports = {
-	
-};
